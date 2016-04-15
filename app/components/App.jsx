b9435a7794dc4a35c204c2aa57e2eabc8300e64f
Rhio Kim <rhio.kim@gmail.com>
2016-04-15 18:10:54 +0900

    feat: Implement basic app and sample react.js component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
+import { Button } from './ui';
+import Header from './Header';
+import Footer from './Footer';
 
 require('./App.css');
+require('./Header.css');
+require('./Footer.css');
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleClick1.bind(this);
+    this.handleClick2.bind(this);
+  }
+
+  handleClick1() {
+    // alert('Clicked Button1');
+  }
+
+  handleClick2() {
+    // alert('Clicked Button2');
   }
 
   render() {
     return (
-      <h1>Hello World</h1>
+      <div>
+        <Header />
+        <div>
+          <h1>Hello World</h1>
+          <Button value="Button1" onClick={this.handleClick1} /> <Button value="Button2" onClick={this.handleClick2} />
+        </div>
+        <Footer />
+      </div>
     );
   }
 }
