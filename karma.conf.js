eab7a00c5603eed975990b66b624ee8919b01d0e
rhiokim <rhio.kim@gmail.com>
2016-04-23 15:54:26 +0900

    Add karma phantom launcher

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,15 @@ module.exports = function (config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '.',
 
-    browsers: [ 'Chrome' ],
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: [ 'PhantomJS', 'Chrome' ],
 
     // karma only needs to know about the test bundle
     files: [
@@ -31,7 +39,10 @@ module.exports = function (config) {
       'tests.bundle.js': [ 'webpack', 'sourcemap', 'coverage' ]
     },
 
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage',
     reporters: [ 'progress', 'jenkins', 'coverage', 'dots' ],
+
     coverageReporter: {
       dir: 'coverage/',
       reporters: [
@@ -59,6 +70,10 @@ module.exports = function (config) {
     // },
     webpackMiddleware: {
       noInfo: true,
-    }
+    },
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO
   });
 };
