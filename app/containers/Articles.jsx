0794b71a056cc0fc1a8cc013d73ff001340c9777
rhiokim <rhio.kim@gmail.com>
2016-06-20 01:39:37 +0900

    Feat: add progress example

--- app/containers/Articles.jsx
+++ app/containers/Articles.jsx
@@ -1,18 +1,27 @@
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 import * as ArticleActions from 'actions/article';
 import ArticleList from 'components/articles/ArticleList';
+import { LinearProgress } from 'react-md/lib/Progress';
 
 class Articles extends Component {
   constructor(props) {
     super(props);
 
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      isFetching: false
+    };
   }
 
   componentWillMount() {
+    this.setState({
+      isFetching: true
+    });
     this.props.fetchArticles();
   }
 
@@ -20,15 +29,22 @@ class Articles extends Component {
   }
 
   componentWillReceiveProps() {
+    this.setState({
+      isFetching: false
+    });
   }
 
   handleChange() {
   }
 
   render() {
+    const {isFetching} = this.state;
     const {articles} = this.props;
     return (
-      <ArticleList articles={articles} />
+      <div>
+        {isFetching && <LinearProgress key="progress" />}
+        <ArticleList articles={articles} />
+      </div>
     );
   }
 }
